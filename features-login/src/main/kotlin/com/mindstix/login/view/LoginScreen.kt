d8dce3f1bcd95a305fabfa4a0a9e41a4f8103026
Asim Shah <asim.shah@mindstix.com>
2023-12-10 03:21:48 +0530

    Updated Login Screen

--- features-login/src/main/kotlin/com/mindstix/login/view/LoginScreen.kt
+++ features-login/src/main/kotlin/com/mindstix/login/view/LoginScreen.kt
@@ -5,17 +5,44 @@
 
 package com.mindstix.login.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import com.mindstix.login.intents.LoginIntent
 import com.mindstix.login.intents.LoginViewStates
 
@@ -26,7 +53,7 @@ import com.mindstix.login.intents.LoginViewStates
  * @param keyboardController The software keyboard controller.
  * @param userIntent A function to handle user intents related to the Login Screen.
  *
- * @author Abhijeet Kokane
+ * @author Abhijeet Kokane, Asim Shah
  */
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -46,13 +73,87 @@ fun LoginScreen(
         // Display the screen title
         Text(state.data.screenTitle)
 
-        // Button to navigate to the Home Screen
-        Button(onClick = {
-            keyboardController?.hide()
-            // Invoke the user intent to navigate to the Home Screen
-            userIntent.invoke(LoginIntent.NavigateToHomeScreen)
-        }) {
-            Text(state.data.loginCtaLabel)
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var isPasswordVisible by remember { mutableStateOf(false) }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            // Logo
+            Image(
+                painter = rememberVectorPainter(image = Icons.Default.Person),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Email TextField
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                label = { Text("Email") },
+                leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { /* Handle next action */ }
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Password TextField
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                label = { Text("Password") },
+                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = null) },
+                trailingIcon = {
+                    // Password visibility toggle
+                    IconButton(
+                        onClick = { isPasswordVisible = !isPasswordVisible }
+                    ) {
+                        Icon(
+                            if (isPasswordVisible) Icons.Outlined.Lock else Icons.Filled.Lock,
+                            contentDescription = if (isPasswordVisible) "Hide Password" else "Show Password"
+                        )
+                    }
+                },
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { /* Handle login action */ }
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Login Button to navigate to the Home Screen
+            Button(onClick = {
+                keyboardController?.hide()
+                // Invoke the user intent to navigate to the Home Screen
+                userIntent.invoke(LoginIntent.NavigateToHomeScreen)
+            }) {
+                Text(state.data.loginCtaLabel)
+            }
         }
     }
 }
