796220a17e27897caae091133450caa6e1364fe4
Apoorv Gupta - Mindstix <apoorv.gupta@mindstix.com>
2024-09-27 14:09:05 +0530

    Screenshot saving added

--- features-floating-view/src/main/kotlin/com/mindstix/floatingview/view/ExpandedComposeView.kt
+++ features-floating-view/src/main/kotlin/com/mindstix/floatingview/view/ExpandedComposeView.kt
@@ -1,33 +1,63 @@
 package com.mindstix.floatingview.view
 
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * @author Apoorv Gupta
  */
 
 @Composable
-fun ExpandedComposeView(popBack: () -> Unit = {}) {
+fun ExpandedComposeView(
+    popBack: () -> Unit = {},
+    expand: () -> Unit = {}
+) {
+
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
 
     Column(modifier = Modifier) {
         Column(
             modifier =
-                Modifier
-                    .size(200.dp)
-                    .background(Color.LightGray),
+            Modifier
+                .width(400.dp)
+                .height(100.dp)
+                .background(Color.LightGray),
         ) {
             Row {
                 Button(onClick = { popBack() }) {
                     Text(text = "pop back!")
+                }
+
+                Button(
+                    onClick = {
+                        val intent = Intent(context, ScreenshotActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                        context.startActivity(intent)
+
+                        popBack.invoke()
+                        coroutineScope.launch {
+                            delay(750)
+                            expand.invoke()
+                        }
+                    }
+                ) {
+                    Text("Start Screen Capture")
                 }
             }
         }
---
+++ features-floating-view/src/main/kotlin/com/mindstix/floatingview/view/ScreenshotActivity.kt
@@ -0,0 +1,203 @@
+package com.mindstix.floatingview.view
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.ImageReader
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.DisplayMetrics
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
+import com.mindstix.core.logger.Logger
+import com.mindstix.floatingview.service.ScreenCaptureService
+
+/**
+ * @author Apoorv Gupta
+ */
+
+
+class ScreenshotActivity : ComponentActivity() {
+
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+    private var mediaProjection: MediaProjection? = null
+    private var virtualDisplay: VirtualDisplay? = null
+    private lateinit var imageReader: ImageReader
+    private val SCREEN_CAPTURE_REQUEST_CODE = 1001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startScreenCaptureService(SCREEN_CAPTURE_REQUEST_CODE, Intent())
+        startScreenCapture()
+    }
+
+    private fun startScreenCapture() {
+        val captureIntent = mediaProjectionManager.createScreenCaptureIntent()
+        screenCaptureLauncher.launch(captureIntent)
+    }
+
+    private fun startScreenCaptureService(resultCode: Int, data: Intent) {
+        val serviceIntent = Intent(this, ScreenCaptureService::class.java).apply {
+            putExtra(ScreenCaptureService.SCREEN_CAPTURE_RESULT_CODE, resultCode)
+            putExtra(ScreenCaptureService.SCREEN_CAPTURE_INTENT_DATA, data)
+        }
+        startForegroundService(serviceIntent)
+    }
+
+    private var screenCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            startMediaProjection(result.resultCode, result.data!!)
+        } else {
+            Toast.makeText(this, "Screen capture permission denied. Activity", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startMediaProjection(resultCode: Int, data: Intent) {
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data)
+
+        // Register a callback to handle MediaProjection lifecycle
+        mediaProjection?.registerCallback(object : MediaProjection.Callback() {
+            override fun onStop() {
+                super.onStop()
+                Logger.d { "ScreenCapture MediaProjection stopped" }
+                virtualDisplay?.release()
+                mediaProjection = null
+            }
+        }, null)  // Null handler means callback will run on the main thread
+
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+
+        imageReader = ImageReader.newInstance(metrics.widthPixels, metrics.heightPixels, PixelFormat.RGBA_8888, 2)
+
+        virtualDisplay = mediaProjection?.createVirtualDisplay(
+            "ScreenCapture",
+            metrics.widthPixels,
+            metrics.heightPixels,
+            metrics.densityDpi,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            imageReader.surface,
+            null,
+            null,
+        )
+
+        captureImage()
+    }
+
+    private fun captureImage() {
+        val handlerThread = HandlerThread("ImageCaptureThread")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+        imageReader.setOnImageAvailableListener({ reader ->
+            val image = reader.acquireNextImage()
+            if (image != null) {
+                // Process the image
+                val planes = image.planes
+                val buffer = planes[0].buffer
+                val width = image.width
+                val height = image.height
+                val pixelStride = planes[0].pixelStride
+                val rowStride = planes[0].rowStride
+                val rowPadding = rowStride - pixelStride * width
+
+                val bitmap = Bitmap.createBitmap(
+                    width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888
+                )
+                bitmap.copyPixelsFromBuffer(buffer)
+
+                // Update UI in Jetpack Compose
+                bitmapState.value = bitmap
+
+                // Save the bitmap to a file (optional)
+                saveBitmapToFile(bitmap)
+
+                Logger.d { "ScreenshotActivity Screenshot captured!" }
+
+                image.close()  // Don't forget to close the image!
+
+                // close activity after taking screenshot
+                finish()
+
+            } else {
+                Logger.d { "ScreenshotActivity No image available" }
+            }
+        }, handler)  // Make sure to run this on a handler if needed
+    }
+
+    private fun saveBitmapToFile(bitmap: Bitmap) {
+        try {
+            val fileOutputStream = openFileOutput("screenshot.png", Context.MODE_PRIVATE)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
+            fileOutputStream.close()
+            Toast.makeText(this, "Screenshot saved successfully", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaProjection?.stop()
+        virtualDisplay?.release()
+    }
+
+    companion object {
+        val bitmapState = mutableStateOf<Bitmap?>(null)
+    }
+}
+
+@Composable
+fun ScreenCaptureApp(onCaptureClick: () -> Unit) {
+    val bitmap by remember { ScreenshotActivity.bitmapState }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { onCaptureClick() }) {
+            Text("Capture Screenshot")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        bitmap?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = "Captured Screenshot",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+            )
+        }
+    }
+}
